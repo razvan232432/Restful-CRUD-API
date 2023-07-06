bf8254a4ee61ceb11f6c3908767b23587c7e8f68
razvan232432 <marius3837@proton.me>
2023-07-06 04:30:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // routes
@@ -7,7 +8,13 @@ app.get('/',(req, res) => {
     res.send('Hello');
 ;})
 
+app.get('/blog',(req, res) => {
+    res.send('Hello  blog');
+;})
+
 app.listen(3000, () => {
     console.log('node os running on  port  3000');
 });
+
+// set   default  mongoose connection
 
