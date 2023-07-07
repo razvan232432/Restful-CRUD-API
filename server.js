a2331d6454d133deb676bc3c432d07eeba08ddd3
razvan232432 <marius3837@proton.me>
2023-07-07 04:34:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ app.get('/',(req, res) => {
 app.get('/blog',(req, res) => {
     res.send('Hello  blog');
 ;})
-
-
-
-// set   default  mongoose connection
-
-mongoose.connect('mongodb://localhost:27017');
-app.listen(3000, () => {
-    console.log('node is running on port 3000')
-})
-.then(() => {
-    console.log('connected to MongoDB');
-}).catch((error) => {
-    console.log(error);
-});
+ app.listen(3000, () => {
+   console.log('node is running on port 3000');
+ });
