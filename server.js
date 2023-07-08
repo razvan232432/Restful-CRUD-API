076e7c94ca2544ddd1b07c7c9214e2571e914f57
razvan232432 <marius3837@proton.me>
2023-07-08 03:48:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.get('/',(req, res) => {
 app.get('/blog',(req, res) => {
     res.send('Hello  blog');
 ;})
- app.listen(3000, () => {
-   console.log('node is running on port 3000');
- });
+
+
+
+ mongoose.connect('mongodb+srv://marius3837:123Qwe12r+@cluster0.afi4oij.mongodb.net/?retryWrites=true&w=majority')
+    .then(() =>{
+      console.log('connected to MMongoDB');
+      app.listen(3000, () => {
+      console.log('node is running on port 3000');
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
+
+ 
