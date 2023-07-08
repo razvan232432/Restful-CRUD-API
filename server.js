369ca96036e2fdf45cf12b36d4cb1b28654e6007
razvan232432 <marius3837@proton.me>
2023-07-08 04:09:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ app.get('/blog',(req, res) => {
     res.send('Hello  blog');
 ;})
 
+app.get('/products', async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
+app.get('/products/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const product= await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
 app.post('/product', async(req, res) => {
    try{
     const product = await Product.create(req.body);
